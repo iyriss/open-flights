b35b18ecc2cde2ca2f2a558cc185e7e12a498993
Iyris <iyrisvigil@gmail.com>
2020-10-25 21:08:57 -0700

    fix typo missing colons

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,11 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :airlines, param: slug #we'll use slug instead of using id as the primary param for airlines in routing
+      resources :airlines, param: :slug #we'll use slug instead of using id as the primary param for airlines in routing
       resources :reviews, only: [:create, :destroy]
     end
   end
 
   #This route requests that aren't for existing paths predefined in our API back to our index path so this will allow React Router to handle routing to our react components without interfering with our actual rails routes for our API
-  get '*path', to: 'pages#index', via :all
+  get '*path', to: 'pages#index', via: :all
 end
