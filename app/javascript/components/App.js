44416e74ecea80a992e5db6eaae231b45c0e9347
Iyris <iyrisvigil@gmail.com>
2020-10-26 00:24:19 -0700

    add React Router and test it with a simple Airline and Airlines components

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+import Airline from './Airline/Airline'
+import Airlines from './Airlines/Airlines'
 
 const App = () => {
-  return (<div> It's happening! Oh and hello world for the 1000th time</div>)
+  return (
+  <Switch>
+    <Route exact path="/" component={Airlines}/>
+    <Route exact path="/airlines/:slug" component={Airline}/> {/* #this will point to an individual component  */}
+  </Switch>
+  )
 }
 
 export default App;
